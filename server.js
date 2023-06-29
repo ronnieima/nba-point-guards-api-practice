b20b7cbf89f2a428ec774f755f5bafe94281c1fd
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-06-29 20:15:03 +0200

    update apackage

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('/api/:name', (request, response) =>{
     }
 })
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
     console.log(`The server is running on port ${PORT}!`)
 })
 
