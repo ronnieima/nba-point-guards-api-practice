ab12555cb850aef5fb471710365beed402c7ca60
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-06-30 14:36:10 +0200

    update visibility

--- public/main.js
+++ public/main.js
@@ -13,7 +13,7 @@ async function sendApiReq(){
         const response = await fetch(`https://good-pink-giraffe-shoe.cyclic.app/api/${teamName}`)
         const data = await response.json()
 
-        console.log(data)
+        document.querySelector('img').style.visibility = visible;
         document.querySelector('#name').innerText = data.pointGuard
         document.querySelector('#team').innerText = data.team
         document.querySelector('img').src = data.photo
